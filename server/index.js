8966aaa8b71f6bf0339a54817b186f03bcac786a
Nidhi <your-nidhiatkes@gmail.com.com>
2026-02-17 04:17:07 +0530

    Added test route

--- server/index.js
+++ server/index.js
@@ -94,4 +94,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => 
   console.log(`Server running on port ${PORT}`)
 );
+app.get("/", (req, res) => {
+  res.send("Backend is working 🚀");
+});
 
