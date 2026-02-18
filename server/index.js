92a67b3a3a2d33c8237cda96c9c5d4aa9b019d9a
Nidhi <your-nidhiatkes@gmail.com.com>
2026-02-18 20:29:39 +0530

    Fixed model name bug

--- server/index.js
+++ server/index.js
@@ -15,16 +15,14 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
+  origin: [
+    "http://localhost:5173",
+    "https://ys-financials.vercel.app"
+  ],
+  methods: ["GET", "POST"],
+  credentials: true
 }));
+
 
 
 
